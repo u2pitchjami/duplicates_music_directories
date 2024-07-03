ee123653d575bd931f4216455ae61ba3fae1a22e
TB <149841209+u2pitchjami@users.noreply.github.com>
2024-07-03 11:08:32 +0200

    cleanup

--- doublons-mp3-flac.sh
+++ doublons-mp3-flac.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 ############################################################################## 
 #                                                                            #
-#	SHELL: !/bin/bash       version 1.0                                  #
+#	SHELL: !/bin/bash       version 1.1                                  #
 #									     #
 #	NOM: BEUGNET							     #
 #									     #
@@ -13,15 +13,14 @@
 #									     #
 ############################################################################## 
 
-#définition des variables
-#BASE=/home/thierry/bidule/
-BASE=/mnt/user/Medias/"Shared Music"/Collection/
+######CONFIGURATION###########################################################
+BASE=/music/base/folder/
 DATE=$(date "+%Y-%m-%d")
-LOG=/mnt/user/Documents/scripts/logs/${DATE}-dossiers_multiformats.txt
-#NBSLASH=5
-#NBSLASH2=6
-NBSLASH=8
-NBSLASH2=9
+LOG=/path/to/logs/folder/${DATE}-dossiers_multiformats.txt
+NBSLASH=8 #num of / for cut command and find artist name in the path
+NBSLASH2=9 #num of / for cut command and find album name in the path
+######CONFIGURATION###########################################################
+
 #création du fichier log
 touch "${LOG}"
 #calcul du nombre de dossiers artistes
@@ -39,23 +38,21 @@ echo "${ARTISTE}"
 #recherche du nombre de dossiers albums pour l'artiste
 NBDIR2=$(find "${DIR}"/* -maxdepth 5 -type d | wc -l)
 echo "Nombre d'albums pour cet artiste : ${NBDIR2}" 
- for ((d=1 ; d<=$NBDIR2 ; d++))
+        for ((d=1 ; d<=$NBDIR2 ; d++))
         do
 	DIR2=$(find "${DIR}"/* -maxdepth 5 -type d | head -$d | tail +$d )
         ALBUM=$(echo "${DIR2}" | rev | cut -d "/" -f1 | rev )
 	echo "${ALBUM}"
-
-#	NBDIR3=$(find "${DIR2}" -maxdepth 1 -mindepth 0 -regex '.*\(mp3\|flac\)$' | wc -l)
+     
 	NBDIR3=$(find "${DIR2}" -maxdepth 1 -mindepth 0 -type d -exec sh -c '[ -f "$0"/*.mp3 ] && [ -f "$0"/*.flac ]' '{}' \; -print | wc -l) 
-       if [ $NBDIR3 -gt 0 ]
-      then
-	#si correspondance trouvée, elle est envoyée dans le fichier log 
-     echo "$NBDIR3 correspondance(s), liste à contrôler ci dessous :" 
-#    DIR3=$(find "${DIR2}" -maxdepth 1 -mindepth 0 -regex '.*\(mp3\|flac\)$' | tee -a ${LOG})
-     DIR3=$(find "${DIR2}" -maxdepth 1 -mindepth 0 -type d -exec sh -c '[ -f "$0"/*.mp3 ] && [ -f "$0"/*.flac ]' '{}' \; -print | tee -a ${LOG})
-    echo $DIR3
-       else
-        echo "OK"
-        fi
-done
+                if [ $NBDIR3 -gt 0 ]
+                then
+	        #si correspondance trouvée, elle est envoyée dans le fichier log 
+                echo "$NBDIR3 correspondance(s), liste à contrôler ci dessous :" 
+                DIR3=$(find "${DIR2}" -maxdepth 1 -mindepth 0 -type d -exec sh -c '[ -f "$0"/*.mp3 ] && [ -f "$0"/*.flac ]' '{}' \; -print | tee -a ${LOG})
+                echo $DIR3
+                else
+                echo "OK"
+                fi
+        done
 done
